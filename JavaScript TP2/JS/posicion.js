a9c3cf9cd685fcc277a0ff019048f807cb138627
Thomas Reynoso <144083966+Thomas45125@users.noreply.github.com>
2024-04-16 23:16:08 -0300

    Update posicion.js

---
+++ JavaScript TP2/JS/posicion.js
@@ -0,0 +1,8 @@
+// Mostrar los números generados junto con su posición en la consola del navegador
+console.log("Arrays aleatorios:");
+array.forEach((numero, indice) => {
+    console.log("Posición " + indice + ": " + numero);
+});
+
+// Mostrar los números generados en la pantalla
+document.write("Arrays aleatorios: " + array);
